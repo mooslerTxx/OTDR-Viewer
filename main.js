4ee056cca9729717f49fa1f8e95bf5cb4d452c12
Marco Moosler <moosler@telemaxx.de>
2020-01-16 16:00:53 +0100

    extract Block Info implemented

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ const path = require('path');
 const Sor = require( path.join(__dirname, './lib/sor.js') );
 
 let arg = process.argv;
+let dir ='./';
+let datapath =dir+'data';
+let filename1 = 'EXFO_FTB7400_1550_U.SOR';
+let filename2 = 'JDSU_MTS6000_1310_G.sor';
+// let filepath1 = path.join(__dirname,'./data/EXFO_FTB7400_1550_U.SOR');
 
-let filepath1 = path.join(__dirname,'./data/EXFO_FTB7400_1550_U.SOR');
-let filepath2 = path.join(__dirname,'./data/JDSU_MTS6000_1310_G.sor');
-
-let filepath = filepath2;
-
+let filepath = datapath+'/'+filename1;
 let filename = path.basename(filepath);
+let basename = path.posix.basename(__dirname);
 let ext = path.extname(filepath).toUpperCase();
-
 if(ext !== '.SOR'){
     throw 'only Files with file extension ".sor" allowed'; 
 }
 
-console.log(`Reading File ${filename} in Folder ${filepath}`);
-let sor = new Sor(filepath);
+console.log(`Reading File ${filename} in Folder ${basename}`);
 
+let sor = new Sor(filepath);
 sor.parse();
