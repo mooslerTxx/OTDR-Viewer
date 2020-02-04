ecc6bf41a37c6617318f46aa8e170fcf129fc80d
Marco Moosler <moosler@telemaxx.de>
2020-02-04 15:08:28 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const SorReader = require('./lib/sor.js');
+
+
+let arg = process.argv;
+let dir = './';
+let datapath = dir + 'data';
+let sample1 = 'EXFO_FTB7400_1550_U.SOR';
+let sample2 = 'JDSU_MTS6000_1310_G.sor';
+let sample3 = 'sample1310_lowDR.sor';
+
+let filename = sample3;
+
+let filepath = datapath + '/' + filename;
+let ext = filename.split('.').pop().toLowerCase();
+
+if (ext !== 'sor') {
+    throw 'only Files with file extension ".sor" allowed';
+}
+
+
+let sor = new SorReader(filepath);
+let result = sor.parse();
